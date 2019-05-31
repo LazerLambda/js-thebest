583579c34ec007f5f71188af172bcfa573ab8387
lampyridae <lampyridae2000@gmail.com>
2019-05-31 13:05:53 +0200

    Feld kann initialisiert werden

--- Projekt/src/Game.ts
+++ Projekt/src/Game.ts
@@ -1,49 +1,34 @@
+import { Field } from "./Field";
 
-import * as SGE from './StaticGameElement';
+export class Game {
+  context: any;
+  frameTime: number;
+  then: number;
 
+  constructor() {
+    const canvas = <HTMLCanvasElement>document.getElementById("canvasId");
+    this.context = canvas.getContext("2d");
 
-export class Game{
+    /**
+     * Init field
+     */
+    const field = new Field();
+    field.drawField();
+  }
 
-    context : any;
-    frameTime : number;
-    then : number;
+  startAnimating(targetFPS: number) {
+    this.frameTime = 1000 / 2;
+    this.then = window.performance.now();
+    this.animate(this.then);
+  }
 
-    field : SGE.StaticGameElement[];
-
-
-    constructor(){
-        const canvas = <HTMLCanvasElement>document.getElementById('canvasId');
-        this.context = canvas.getContext('2d');
-        new SGE.StaticGameElement(1,1, this.context).draw();
-        new SGE.StaticGameElement(1,2, this.context).draw();
-        new SGE.StaticGameElement(2,1, this.context).draw();
-        
-        const fieldSize_X : number = 50;
-        const fieldSize_Y : number = 25;
-
-        for(let i = 0; i < fieldSize_X; i++){
-            for(let j = 0; j < fieldSize_Y; j++){
-
-            }
-        }
-    }
-
-    
-    startAnimating(targetFPS : number) {
-        this.frameTime = 1000 / 2;
-        this.then = window.performance.now();
-        this.animate(this.then);
-    }
-    
-    animate(currentTime: number) {
-        window.requestAnimationFrame(this.animate.bind(this));
-        const now = currentTime;
-        const elapsed = now - this.then;
+  animate(currentTime: number) {
+    window.requestAnimationFrame(this.animate.bind(this));
+    const now = currentTime;
+    const elapsed = now - this.then;
 
     if (elapsed > this.frameTime) {
-        this.then = now;
-
-      }
+      this.then = now;
     }
- 
+  }
 }
