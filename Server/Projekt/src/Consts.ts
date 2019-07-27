554f9512b29d363c148b7b8a33122bc8d08f267d
ru38kiz <p.koch@campus.lmu.de>
2019-07-27 23:11:14 +0200

    Update Consts.ts

--- Server/Projekt/src/Consts.ts
+++ Server/Projekt/src/Consts.ts
@@ -1,7 +1,6 @@
-
 export module Consts{
     export const ARRAY_CONST : number = 8; 
-    export const MAX_PLAYERS : number = 2;
+    export const MAX_PLAYERS : number = 4;
     export const NUKE_RAD : number = 5;
     export const BOMB_RAD : number = 3;
     export const TIMEOUT_LENGTH: number = 20;
